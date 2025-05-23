b8097ff1e44ced5ad316c9b107f95c0254565b51
Sonhong1805 <nguyenhongson18052003@gmail.com>
2025-05-23 22:43:30 +0700

    try deploy 4 time

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      external: ["slate-dom"],
+    },
+  },
 });
